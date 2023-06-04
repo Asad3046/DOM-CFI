c34a57a21471a83ba9bda9fc3e7380941778cb9d
Asad3046 <asadmohammad8259@gmail.com>
2023-06-04 18:42:16 +0530

    first commit

---
+++ dom/script.js
@@ -0,0 +1,58 @@
+// let myVar = "2";
+//console.log(myVa);
+
+// let name = prompt("please enter your name")
+
+// console.log(name);
+//alert(name);
+
+//console.log((4/3).toFixed(4));
+// console.log(4%3);
+// let a = 46;
+// a++;
+// a++;
+// --a;
+// a += 10;
+// a *= 25;
+
+// console.log(a);
+
+// function fun(a,b) {
+//     console.log(a*b);
+// }
+
+// fun(2,3)
+
+// function buyingChocolate(a)
+
+// function r (){
+//     let random = Math.random()*6;
+//     return Math.floor(random +1 );
+
+// }
+// let output = r ()
+// console.log(output)
+
+// function largest(){
+//     if
+// }
+
+let students =[asad, ali, humayun, wasif, hanzala];
+
+function check (){
+    let studentsName = prompt("Enter your name");
+        for(let i = 0; i < students.length; i++);{
+            if(students[i]==studentsName)
+                console.log("you are welcome");
+                return
+        }
+}
+check()
+
+// let heading = document.querySelector("h1")
+// let button = document.querySelector("#btn")
+
+// button.addEventListener("click", ()=> {
+//     heading.textContent = " hello world"
+
+// })
